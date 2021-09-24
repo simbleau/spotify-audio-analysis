ddc8bdc27ca934ae1628c2028e5c4ca68e41f458
Spencer Imbleau <spencer@imbleau.com>
2021-09-24 17:53:08 -0400

    32.1 on WebCAT

--- pitch.py
+++ pitch.py
@@ -13,8 +13,9 @@ OUTPUT_SIZE = 12
 # Example Output (from https://cs.appstate.edu/~rmp/cs5440/AnalyzeDocumentation.pdf)
 # "pitches": [0.370, 0.067, 0.055, 0.073, 0.108, 0.082, 0.123, 0.180, 0.327, 1.000, 0.178, 0.234]
 layers = [
+    Dense(500, activation='tanh'),
     Dense(500, activation='sigmoid'),
-    Dense(OUTPUT_SIZE, activation='sigmoid') # Should be an activation which clamps values between 0 and 1
+    Dense(OUTPUT_SIZE, activation='softmax') # Should be an activation which clamps values between 0 and 1
 ]
 
 # Loss function
@@ -24,10 +25,10 @@ loss_function = MeanSquaredError()
 optimizer = Adamax(learning_rate=0.1)
 
 # Batch Size
-batch_size = 2048
+batch_size = 4098
 
 # Epochs
-epochs = 20
+epochs = 30
 
 # Run
 if __name__ == '__main__':
