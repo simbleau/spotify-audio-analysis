b7ac637840b76cfb9f69c0df732b3b324a1f7418
Spencer Imbleau <spencer@imbleau.com>
2021-09-24 17:19:54 -0400

    38.2 on WebCAT

--- timbre.py
+++ timbre.py
@@ -9,21 +9,25 @@ from tensorflow.keras.layers import *
 OUTPUT_SIZE = 12
 
 # Model
+#
+# Example Output (from https://cs.appstate.edu/~rmp/cs5440/AnalyzeDocumentation.pdf)
+# "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
-    Dense(OUTPUT_SIZE, activation='linear')
+    Dense(500, activation='linear'),
+    Dense(OUTPUT_SIZE, activation='linear') # Should be an activation which allows negative values
 ]
 
 # Loss function
 loss_function = MeanSquaredError()
 
 # Optimizers
-optimizer = Adamax(learning_rate=0.01)
+optimizer = Adamax(learning_rate=0.1)
 
 # Batch Size
-batch_size = 512
+batch_size = 2048
 
 # Epochs
-epochs = 1
+epochs = 100
 
 # Run
 if __name__ == '__main__':
