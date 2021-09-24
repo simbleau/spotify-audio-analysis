282ca598d95bb6efa59af6178fc972998ebaee40
Spencer Imbleau <spencer@imbleau.com>
2021-09-24 16:07:52 -0400

    Sugary script which prints the best model's loss

---
+++ best_models.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from helper_methods import *
+
+from os.path import exists
+
+
+def print_best_model(path, endpoint):
+    # Get best model and print out the loss
+    if exists(path):
+        # Get validation data
+        x_valid, y_valid = get_xy('data/spotify_valid.npz', endpoint)
+        model = keras.models.load_model(path, compile=True, custom_objects={'Normalization': Normalization})
+        val_loss = model.evaluate(x_valid, y_valid, verbose=0)
+        print(f"{endpoint}: {val_loss} loss")
+    else:
+        print(f"No best model for {endpoint}")
+
+
+def run(endpoint, layers, loss_function, optimizer, batch_size, epochs, save):
+    # Setup path for artifacts
+    timbre_model_path = f'best_models/timbre.h5'
+    pitch_model_path = f'best_models/pitch.h5'
+    loudness_model_path = f'best_models/loudness.h5'
+
+    print_best_model(timbre_model_path, 'timbre')
+    print_best_model(pitch_model_path, 'pitch')
+    print_best_model(loudness_model_path, 'loudness')
