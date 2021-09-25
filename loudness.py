7455aedf32871e3cae23614fbda8cb8caa1b7f43
Spencer Imbleau <spencer@imbleau.com>
2021-09-25 09:33:48 -0400

    Removed unnecessary variable

--- loudness.py
+++ loudness.py
@@ -6,7 +6,6 @@ from tensorflow.keras.optimizers import *
 from tensorflow.keras.losses import *
 from tensorflow.keras.layers import *
 
-OUTPUT_LAYER = Dense(1, activation='linear')  # Should be an activation which allows negative values
 
 # Model
 #
