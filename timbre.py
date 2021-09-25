b1743aa854136630f0b099b7ffcfd700a11ca0bc
Spencer Imbleau <spencer@imbleau.com>
2021-09-25 10:01:47 -0400

    67.7 on WebCAT

--- timbre.py
+++ timbre.py
@@ -11,7 +11,7 @@ from tensorflow.keras.layers import *
 # Example Output (from https://cs.appstate.edu/~rmp/cs5440/AnalyzeDocumentation.pdf)
 # "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
-    Dense(500, activation='linear'),
+    Dense(500, activation='sigmoid'),
 ]
 
 # Loss function
@@ -21,7 +21,7 @@ loss_function = MeanSquaredError()
 optimizer = Adamax(learning_rate=0.1)
 
 # Batch Size
-batch_size = 2048
+batch_size = 1024
 
 # Epochs
 epochs = 100
