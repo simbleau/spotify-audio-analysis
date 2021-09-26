1e97ee1f02dcd65d218bef921f147dccdafb9898
Danielle Lapensee-Rankine <lapenseerankinedl@appstate.edu>
2021-09-26 15:10:12 -0400

    68.0 on WebCat

--- timbre.py
+++ timbre.py
@@ -11,7 +11,7 @@ from tensorflow.keras.layers import *
 # Example Output (from https://cs.appstate.edu/~rmp/cs5440/AnalyzeDocumentation.pdf)
 # "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
-    Dense(500, activation='sigmoid'),
+    Dense(575, activation='sigmoid'),
 ]
 
 # Loss function
