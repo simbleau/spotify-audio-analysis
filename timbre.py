4af692aff2fbce1eb40e4f97efa34c15ac402088
AndrewR08 <AndrewR08@users.noreply.github.com>
2021-09-28 18:06:23 -0400

    68.1 on WebCat

--- timbre.py
+++ timbre.py
@@ -12,6 +12,7 @@ from tensorflow.keras.layers import *
 # "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
     Dense(500, activation='sigmoid'),
+    Dense(1000, activation='sigmoid')
 ]
 
 # Loss function
