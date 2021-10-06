50cb0cde05d406bd50bded31b69c0cd792c8014e
Spencer Imbleau <spencer@imbleau.com>
2021-10-06 09:04:10 -0400

    Best model updated

--- timbre.py
+++ timbre.py
@@ -12,8 +12,7 @@ from tensorflow.keras.layers import *
 # "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
     Dense(500, activation='sigmoid'),
-    Dense(100, activation='sigmoid'),
-    Dense(250, activation='sigmoid'),
+    Dense(500, activation='sigmoid'),
     Dense(500, activation='sigmoid')
 ]
 
@@ -24,7 +23,7 @@ loss_function = MeanSquaredError()
 optimizer = Adamax(learning_rate=0.1)
 
 # Batch Size
-batch_size = 4098
+batch_size = 2048
 
 # Epochs
 epochs = 1000
