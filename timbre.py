fa5125c322f63ffd1598bab76484bfc945188fed
Spencer Imbleau <spencer@imbleau.com>
2021-10-06 09:33:33 -0400

    Updated batch size

--- timbre.py
+++ timbre.py
@@ -11,9 +11,10 @@ from tensorflow.keras.layers import *
 # Example Output (from https://cs.appstate.edu/~rmp/cs5440/AnalyzeDocumentation.pdf)
 # "timbre": [24.736, 110.034, 57.822, -171.580, 92.572, 230.158, 48.856, 10.804, 1.371, 41.446, -66.896, 11.207]
 layers = [
-    Dense(500, activation='sigmoid'),
-    Dense(500, activation='sigmoid'),
-    Dense(500, activation='sigmoid')
+    Dense(1500, activation='sigmoid'),
+    Dense(1200, activation='sigmoid'),
+    Dense(1000, activation='relu'),
+    Dense(500, activation='relu')
 ]
 
 # Loss function
@@ -23,7 +24,7 @@ loss_function = MeanSquaredError()
 optimizer = Adamax(learning_rate=0.1)
 
 # Batch Size
-batch_size = 2048
+batch_size = 1024
 
 # Epochs
 epochs = 1000
